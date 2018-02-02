d13a394c6bbcbb7abb7e1d8edcf247c94b639c37
Dmitry Shestakov <altais@ukr.net>
2018-02-02 19:57:13 +0200

    heap manager enabled

--- config/chconf.h
+++ config/chconf.h
@@ -271,7 +271,7 @@
  *
  * @note    The default is @p TRUE.
  */
-#define CH_CFG_USE_MEMCORE                  FALSE
+#define CH_CFG_USE_MEMCORE                  TRUE
 
 /**
  * @brief   Heap Allocator APIs.
@@ -283,7 +283,7 @@
  *          @p CH_CFG_USE_SEMAPHORES.
  * @note    Mutexes are recommended.
  */
-#define CH_CFG_USE_HEAP                     FALSE
+#define CH_CFG_USE_HEAP                     TRUE
 
 /**
  * @brief   Memory Pools Allocator APIs.
