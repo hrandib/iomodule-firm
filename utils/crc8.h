36c4cf11bd1062284cb10a6f950f1f0bf9dd4008
merlokk <807634+merlokk@users.noreply.github.com>
2019-04-22 19:47:40 +0300

    added crc8 (normal one)

--- utils/crc8.h
+++ utils/crc8.h
@@ -9,10 +9,17 @@
 #define CRC8_H_
 
 #include <stdint.h>
+#include <stdlib.h>
+
 extern "C" {
+
 uint8_t crc8_ow(uint8_t *data, uint8_t size);
 uint8_t crc8_ow2(uint8_t crc, uint8_t data);
 uint8_t crc8(uint8_t *data, uint8_t size);
 uint8_t crc8_single(uint8_t data);
+
+// maxim
+uint8_t crc8mx(uint8_t crc, uint8_t *mem, size_t len);
+
 }
 #endif /* CRC8_H_ */
